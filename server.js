290dba4d698d1e0978d673e0e49b1fbfd50a479e
Devante Gandy <vante@Devantes-MacBook-Pro.local>
2021-05-10 15:19:41 -0400

    merge

---
+++ server.js
@@ -0,0 +1,17 @@
+
+//DEPENDENCIES
+const express = require('express');
+// const mongoose = require('mongoose');
+
+//CONFIGGURATION
+const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT
+
+
+
+
+//LISTENER
+app.listen(PORT, () => {
+    console.log('listening on Port', PORT)
+})
